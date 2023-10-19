541a0c072d55cc4bb2056520b040ddd0e2751ab0
jsperger <j.sperger@gmail.com>
2023-10-18 23:57:07 -0400

    Documentation and input checks.

--- R/calculate_power.R
+++ R/calculate_power.R
@@ -39,36 +39,54 @@ test_df,
 
 #' @title Internal Power Calculation Function
 #'
+#' Internal Power Calculation Function: .CalcPower
+#'
 #' @description
-#' Perform the GEE-based power calculation for stepped wedge trials and
-#' multilevel cluster randomized trials. This function is the internal function
-#' for performing the power calculation. It is not exported.
+#' The function \code{.CalcPower} performs the core power calculation for stepped wedge trials and
+#' multilevel cluster randomized trials using Generalized Estimating Equations (GEE).
+#' This function is not intended to be directly called by the user, but rather serves as
+#' an internal function for the main power calculation function.
 #'
 #' @param design_mat_list A list of design matrices, one for each cluster.
+#' @param working_cor_mat_list A list of working correlation matrices, one for each cluster.
 #' @param incidence_mat_list A list of incidence matrices, one for each cluster.
-#' @param cor_mat_list A list of correlation matrices, one for each cluster.
-#' @param trt_param_col_vec A vector of treatment parameters as
-#' a matrix with one column.
+#' @param trt_param_col_vec A column vector of treatment parameters.
 #' @param dispersion_scalar The dispersion parameter for the GEE model.
+#' @param var_fun A function defining the variance as a function of the mean.
 #' @param link The link function for the GEE model.
-#' @param dist The distribution for the GEE model.
-#' @param alpha The desired type I error rate.
+#' @param response_type Type of the response variable (e.g., continuous, count, binary).
+#' @param alpha The desired Type I error rate.
 #' @param test_df The degrees of freedom for the test statistic.
-#' @param power_only_flag A logical flag indicating whether to return only the
-#' calculated power. Used internally for sample size calculations
+#' @param contrast_mat A matrix specifying the contrasts for hypothesis testing.
+#' @param null_val_vec A vector specifying the null hypothesis values for the contrasts.
+#' @param power_only_flag A logical flag indicating whether to return only the calculated power.
+#' Used internally for sample size calculations.
 #'
-#' @return A list with the following components:
-#' \item{power}{The power of the test.}
-#' \item{sample_size}{The sample size required to achieve the desired power.}
-#' \item{test_stat}{The test statistic.}
-#' \item{p_value}{The p-value for the test statistic.}
-#' \item{test_df}{The degrees of freedom for the test statistic.}
+#' @details
+#' The function performs the following calculations:
+#' \enumerate{
+#'   \item Calculates the components for the estimating equation for each cluster: \( \boldsymbol{\mu}_i \),
+#'   \( \textbf{D}_{i} \), \( \textbf{B}_{i} \), \( \textbf{R}_i(\boldsymbol{\alpha}) \), and \( \textbf{V}_{i} \).
+#'   \item Solves for \( \boldsymbol{\Sigma}_1^{-1} \) and \( \widehat{\boldsymbol{\theta}} \).
+#'   \item Uses the Wald test to calculate the power based on the noncentrality parameter \( \lambda \),
+#'   the critical value, and the degrees of freedom \( q \).
+#' }
 #'
+#' @return
+#' The function returns a list with the following components:
+#' \itemize{
+#'   \item \code{power}: The power of the test.
+#'   \item \code{cluster_sample_size}: The sample size for each cluster.
+#'   \item \code{test_stat}: The test statistic.
+#'   \item \code{alpha}: The Type I error rate.
+#'   \item \code{test_df}: The degrees of freedom for the test statistic.
+#' }
 #'
+#' @keywords internal
+
 .CalcPower <- function(design_mat_list,
                        working_cor_mat_list,
                        incidence_mat_list,
-                       cor_mat_list,
                        trt_param_col_vec,
                        dispersion_scalar,
                        var_fun,
@@ -153,6 +171,28 @@ test_df,
   return(power_calc_results)
 }
 
+#' Create the Expected Response Vector (\( \mu \))
+#'
+#' @description
+#' This internal function calculates the expected response vector \( \mu \) based on the design matrix,
+#' treatment parameters, response type, and link function.
+#'
+#' @param design_mat A numeric matrix representing the design of the experiment for a particular cluster.
+#' @param trt_param_vec A numeric vector containing the treatment parameters.
+#' @param response_type A string indicating the type of the response variable (e.g., 'count', 'binary').
+#' @param link A string specifying the link function to use (e.g., 'identity', 'logit', 'log').
+#'
+#' @details
+#' The expected response \( \mu \) is calculated as:
+#' \[
+#' \mu = g^{-1}(X \beta)
+#' \]
+#' where \( g^{-1} \) is the inverse of the link function, \( X \) is the design matrix, and \( \beta \) is the treatment parameters.
+#'
+#' @return
+#' Returns a numeric column vector \( \mu \) containing the expected responses.
+#'
+#' @keywords internal
 .CreateMuiVector <- function (design_mat,
                              trt_param_vec,
                              response_type,
@@ -175,6 +215,25 @@ test_df,
   return(mu_col_vec)
 }
 
+#' Create the Mean Jacobian Matrix (\( D_i \))
+#'
+#' @description
+#' This internal function calculates the mean Jacobian matrix \( D_i \), which is the derivative of the expected response \( \mu \)
+#' with respect to the treatment parameters \( \beta \), based on the link function.
+#'
+#' @param mu_col_vec A numeric column vector containing the expected responses \( \mu \).
+#' @param link A string specifying the link function to use (e.g., 'identity', 'logit', 'log').
+#'
+#' @details
+#' The mean Jacobian matrix \( D_i \) is calculated based on the link function as follows:
+#' \[
+#' D_i = \frac{\partial \mu}{\partial \beta^T}
+#' \]
+#'
+#' @return
+#' Returns a numeric matrix, the mean Jacobian matrix \( D_i \).
+#'
+#' @keywords internal
 .CreateMeanJacobianMatDi <- function(mu_col_vec, link){
 
   mean_jacobian_mat_di <- switch(link,
@@ -185,12 +244,27 @@ test_df,
   return(mean_jacobian_mat_di)
 }
 
-#' @title Create the variance matrix for the GEE model
+#' Create the Variance Parameter Matrix (\( B_i \))
 #'
 #' @description
-#' Create the variance matrix for the GEE model. This function is not exported.
-#' $$B_i = \phi \diag(v(\mu_{i11}), \ldots, v(\mu_{IJK}) )$$
-#' where $\phi$ is the dispersion parameter and $v(\mu_{ijk})$ is the variance
+#' This internal function calculates the variance parameter matrix \( B_i \) based on the expected responses \( \mu \),
+#' the dispersion parameter \( \phi \), and a variance function \( v \).
+#'
+#' @param mu_col_vec A numeric column vector containing the expected responses \( \mu \).
+#' @param dispersion_scalar A numeric scalar representing the dispersion parameter \( \phi \).
+#' @param var_fun A function defining the variance as a function of the mean.
+#'
+#' @details
+#' The variance parameter matrix \( B_i \) is calculated as:
+#' \[
+#' B_i = \phi \, \text{diag}(v(\mu_{i11}), \ldots, v(\mu_{IJK}))
+#' \]
+#' where \( \phi \) is the dispersion parameter and \( v(\mu_{ijk}) \) is the variance.
+#'
+#' @return
+#' Returns a numeric matrix \( B_i \), the variance parameter matrix.
+#'
+#' @keywords internal
 .CreateVarParamMatrixBi <- function(mu_col_vec,
                                     dispersion_scalar,
                                     var_fun){
@@ -205,6 +279,25 @@ test_df,
   return(var_mat_bi)
 }
 
+#' Calculate the Covariance Matrix (\( V_i \))
+#'
+#' @description
+#' This internal function calculates the covariance matrix \( V_i \) based on the variance parameter matrix \( B_i \)
+#' and the working correlation matrix \( R_i \).
+#'
+#' @param var_mat_bi A numeric matrix representing the variance parameter matrix \( B_i \).
+#' @param working_cor_mat_ri A numeric matrix representing the working correlation matrix \( R_i \).
+#'
+#' @details
+#' The covariance matrix \( V_i \) is calculated as:
+#' \[
+#' V_i = B_i^{1/2} R_i B_i^{1/2}
+#' \]
+#'
+#' @return
+#' Returns a numeric matrix \( V_i \), the covariance matrix.
+#'
+#' @keywords internal
 .CalcVarMatrixVi <- function(var_mat_bi,
                              working_cor_mat_ri){
 
@@ -214,6 +307,26 @@ test_df,
 
   return(var_mat_vi)
 }
+
+#' Calculate the Model-Based Variance Matrix
+#'
+#' @description
+#' This internal function calculates the model-based variance matrix based on the list of mean Jacobian matrices \( D_i \)
+#' and the list of covariance matrices \( V_i \).
+#'
+#' @param mean_jacobian_mat_list A list of numeric matrices representing the mean Jacobian matrices \( D_i \) for each cluster.
+#' @param var_mat_vi_list A list of numeric matrices representing the covariance matrices \( V_i \) for each cluster.
+#'
+#' @details
+#' The model-based variance matrix is calculated as:
+#' \[
+#' \Sigma_1^{-1} = \left[ \sum_{i = 1}^I D_{i}^T V_{i}^{-1}D_{i} \right]^{-1}
+#' \]
+#'
+#' @return
+#' Returns a numeric matrix representing the model-based variance matrix.
+#'
+#' @keywords internal
 
 .CalcModelBasedVariance <- function(mean_jacobian_mat_list,
                                     var_mat_vi_list){
@@ -234,39 +347,91 @@ test_df,
   return(model_based_var_mat)
 }
 
+
+#' Calculate the Estimated Parameter Vector (\( \hat{\theta} \))
+#'
+#' @description
+#' This internal function calculates the estimated parameter vector \( \hat{\theta} \)
+#' based on the design matrices, mean Jacobian matrices, covariance matrices, expected responses,
+#' and the inverse link function.
+#'
+#' @param design_mat_list A list of numeric matrices representing the design matrices \( X_i \) for each cluster.
+#' @param mean_jacobian_mat_list A list of numeric matrices representing the mean Jacobian matrices \( D_i \) for each cluster.
+#' @param var_mat_vi_list A list of numeric matrices representing the covariance matrices \( V_i \) for each cluster.
+#' @param mui_vec_list A list of numeric vectors representing the expected responses \( \mu_i \) for each cluster.
+#' @param invlink_fun A function representing the inverse link function.
+#'
+#' @details
+#' The estimated parameter vector \( \hat{\theta} \) is calculated as:
+#' \[
+#' \hat{\theta} = \left( \sum_{i = 1}^I X_i^T D_i V_i D_i^T X_i \right)^{-1} \sum_{i = 1}^I X_i^T D_i V_i D_i^T g(\mu_i)
+#' \]
+#' where \( X_i \) are the design matrices, \( D_i \) are the mean Jacobian matrices, \( V_i \) are the covariance matrices,
+#' and \( g(\mu_i) \) are the expected responses transformed by the inverse link function.
+#'
+#' @return
+#' Returns a numeric vector \( \hat{\theta} \), the estimated parameter vector.
+#'
+#' @keywords internal
 .CalcEstimatedParamVec <- function(design_mat_list,
                                    mean_jacobian_mat_list,
                                    var_mat_vi_list,
                                    mui_vec_list,
-                                   invlink_fun){
-  n_clust <- length(design_mat_list)
+                                   invlink_fun) {
 
-  shared_term_list <- lapply(1:n_clust,
-                             function(i){
-                               t(design_mat_list[[i]]) %*%
-                                 mean_jacobian_mat_list[[i]] %*%
-                                 var_mat_vi_list[[i]] %*%
-                                 t(mean_jacobian_mat_list[[i]])
-                             })
+  num_clusters <- length(design_mat_list)
 
-  #TODO: Come up with a more descriptive name for these variables
-left_part_list <- vector(mode = "list", length = n_clust)
-  right_part_list <- vector(mode = "list", length = n_clust)
-  for(i in 1:n_clust){
-    left_part_list[[i]] <- shared_term_list[[i]] %*% design_mat_list[[i]]
-    right_part_list[[i]] <- shared_term_list[[i]] %*% invlink_fun(mui_vec_list[[i]])
+  # Calculate the shared term for each cluster
+  shared_term_for_clusters <- lapply(1:num_clusters,
+                                     function(i) {
+                                       t(design_mat_list[[i]]) %*%
+                                         mean_jacobian_mat_list[[i]] %*%
+                                         var_mat_vi_list[[i]] %*%
+                                         t(mean_jacobian_mat_list[[i]])
+                                     })
+
+  # Initialize lists to hold the left and right parts of the equation
+  left_equation_terms <- vector(mode = "list", length = num_clusters)
+  right_equation_terms <- vector(mode = "list", length = num_clusters)
+
+  # Calculate the left and right parts for each cluster
+  for(i in 1:num_clusters) {
+    left_equation_terms[[i]] <- shared_term_for_clusters[[i]] %*% design_mat_list[[i]]
+    right_equation_terms[[i]] <- shared_term_for_clusters[[i]] %*% invlink_fun(mui_vec_list[[i]])
   }
 
-  left_part_term <- solve(Reduce("+", left_part_list))
+  # Solve for the final left and right terms
+  solved_left_term <- solve(Reduce("+", left_equation_terms))
+  summed_right_term <- Reduce("+", right_equation_terms)
 
-  right_part_term <- Reduce("+", right_part_list)
+  # Calculate the estimated parameter vector by multiplying the left and right terms
+  estimated_param_vector <- solved_left_term %*% summed_right_term
 
-  estimated_param_vec <- left_part_term %*% right_part_term
-
-  return(estimated_param_vec)
+  return(estimated_param_vector)
 }
 
+
 #' @title Calculate the noncentrality parameter for the Wald test
+#'
+#' @description
+#' This internal function calculates the noncentrality parameter \( \lambda \) for the Wald test.
+#'
+#' @param n_clust An integer representing the number of clusters.
+#' @param contrast_mat A numeric matrix representing the contrast matrix \( L \).
+#' @param null_val_vec A numeric vector representing the null value vector \( \ell \).
+#' @param estimated_param_vec A numeric vector representing the estimated parameter vector \( \hat{\theta} \).
+#' @param model_based_var_mat A numeric matrix representing the model-based variance matrix \( \Sigma_1 \).
+#'
+#' @details
+#' The noncentrality parameter \( \lambda \) is calculated as:
+#' \[
+#' \lambda = I \left( L \hat{\theta} - \ell \right)^T \Sigma_1^{-1} \left( L \hat{\theta} - \ell \right)
+#' \]
+#'
+#' @return
+#' Returns a numeric value representing the noncentrality parameter \( \lambda \).
+#'
+#' @keywords internal
 .CalcWaldNcp <- function(n_clust,
                          contrast_mat,
                          null_val_vec,
@@ -283,8 +448,27 @@ left_part_list <- vector(mode = "list", length = n_clust)
 
 }
 
+
+#' Calculate the Critical Value under the Null Hypothesis for the Wald Test
+#'
+#' @description
+#' This internal function calculates the critical value under the null hypothesis for the Wald test.
+#'
+#' @param alpha A numeric value representing the type I error rate \( \alpha \).
+#' @param test_df An integer representing the degrees of freedom.
+#'
+#' @details
+#' The critical value under the null hypothesis is calculated using the chi-squared distribution quantile function:
+#' \[
+#' \chi^2_{1-\alpha, \text{df}}
+#' \]
+#'
+#' @return
+#' Returns a numeric value representing the critical value under the null hypothesis.
+#'
+#' @keywords internal
 .CalcWaldNullCritVal <- function(alpha, test_df){
-  null_crit_val <- qchisq(q = alpha,
+  null_crit_val <- qchisq(q = 1 - alpha,
                      df =  test_df)
 
   return(null_crit_val)
@@ -296,3 +480,54 @@ left_part_list <- vector(mode = "list", length = n_clust)
 ##############################################################################################
 
 #' @title Check the validity of the inputs to the GEE power calculation
+#'
+.CheckCalcPowerInputs <- function(
+  design_mat_list, working_cor_mat_list, incidence_mat_list, trt_param_col_vec,
+  dispersion_scalar, var_fun, link, response_type, alpha, test_df, contrast_mat,
+  null_val_vec, power_only_flag) {
+
+  .CheckNumericVals(alpha = alpha, dispersion_scalar = dispersion_scalar, test_df = test_df)
+
+  .CheckListLengths(design_mat_list = design_mat_list, working_cor_mat_list = working_cor_mat_list, incidence_mat_list = incidence_mat_list)
+
+  .CheckInputTypes(design_mat_list = design_mat_list, working_cor_mat_list = working_cor_mat_list, incidence_mat_list = incidence_mat_list, trt_param_col_vec = trt_param_col_vec, dispersion_scalar = dispersion_scalar, var_fun = var_fun, link = link, response_type = response_type, alpha = alpha, test_df = test_df, contrast_mat = contrast_mat, null_val_vec = null_val_vec, power_only_flag = power_only_flag)
+
+  return(NULL)
+}
+
+.CheckNumericVals <- function(alpha, dispersion_scalar, test_df) {
+  if (alpha <= 0 || alpha >= 1) {
+    stop("ERROR: alpha must be between 0 and 1 exclusive")
+  }
+  if (dispersion_scalar <= 0) {
+    stop("ERROR: dispersion_scalar must be a positive value")
+  }
+  if (test_df <= 0 || !all.equal(test_df %% 1, 0)) {
+    stop("ERROR: test_df must be a positive integer")
+  }
+  return(NULL)
+}
+
+.CheckListLengths <- function(design_mat_list, working_cor_mat_list, incidence_mat_list) {
+  len1 <- length(design_mat_list)
+  len2 <- length(working_cor_mat_list)
+  len3 <- length(incidence_mat_list)
+
+  if (!(len1 == len2 && len2 == len3)) {
+    stop("ERROR: The lengths of design_mat_list, working_cor_mat_list, and incidence_mat_list must be the same")
+  }
+  return(NULL)
+}
+
+.CheckNumericVals <- function(alpha, dispersion_scalar, test_df) {
+  if (alpha <= 0 || alpha >= 1) {
+    stop("ERROR: alpha must be between 0 and 1 exclusive")
+  }
+  if (dispersion_scalar <= 0) {
+    stop("ERROR: dispersion_scalar must be a positive value")
+  }
+  if (test_df <= 0 || !all.equal(test_df %% 1, 0)) {
+    stop("ERROR: test_df must be a positive integer")
+  }
+  return(NULL)
+}
