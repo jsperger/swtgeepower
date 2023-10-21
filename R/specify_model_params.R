c88cde29665509c294a6c5322a81732f85dbb210
jsperger <j.sperger@gmail.com>
2023-10-21 12:43:48 -0400

    Document functions in specify_model_params.R

--- R/specify_model_params.R
+++ R/specify_model_params.R
@@ -7,8 +7,35 @@
 #' @title Specify Data Generating Model
 #'
 #' @description
-#' Wrapper function for specifying the data generating model including calendar time and treatment effects.
-#' @return a numeric vector of model parameters
+#' Wrapper function for specifying the data generating model for a given study.
+#' This function allows for the specification of both calendar time and treatment effects.
+#'
+#' @param n_study_periods Integer. The number of study periods. Must be at least 2.
+#' @param mli_study_flag Logical. Flag to indicate if the study is investigating a multi-level intervention.
+#' @param time_model_type Character. The type of time effect model to be used. Valid values are "linear" and "categorical".
+#' @param time_intercept_param Numeric. The intercept parameter for the time model. Required if `time_model_type` is "linear".
+#' @param time_trend_param Numeric. The trend parameter for the time model. Required if `time_model_type` is "linear".
+#' @param period_effect_param_vec Numeric vector. The period effect parameters for the time model. Required if `time_model_type` is "categorical".
+#' @param treatment_model_type Character. The type of treatment effect model to be used. Valid values are "linear" and "average".
+#' @param clust_trt_param Numeric. The treatment effect parameter at the cluster level.
+#' @param ind_trt_param Numeric. The treatment effect parameter at the individual level. Only required if `mli_study_flag` is TRUE.
+#' @param interaction_param Numeric. The interaction parameter for the treatment effect. Only required if `mli_study_flag` is TRUE.
+#'
+#' @return A named numeric vector of model parameters.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' SpecifyDataGeneratingModel(n_study_periods = 5,
+#'                            mli_study_flag = FALSE,
+#'                            time_model_type = "linear",
+#'                            time_intercept_param = 0.5,
+#'                            time_trend_param = 0.1,
+#'                            treatment_model_type = "linear",
+#'                            clust_trt_param = 0.3)
+#' }
+#'
+#' @export
 SpecifyDataGeneratingModel <- function (n_study_periods = NULL,
                                         mli_study_flag = FALSE,
                                         time_model_type = "linear",
@@ -62,7 +89,17 @@ SpecifyDataGeneratingModel <- function (n_study_periods = NULL,
 ## Individual Effect-type Specification Functions
 ##
 ##############################################################################################
-
+#' @title Specify Linear Time Effect Parameters
+#'
+#' @description
+#' Internal function to specify the parameters for a linear time effect in the data generating model.
+#'
+#' @param time_intercept_param Numeric. The intercept parameter for the time model.
+#' @param time_trend_param Numeric. The trend parameter for the time model.
+#'
+#' @return A named numeric vector with the time effect parameters.
+#'
+#' @keywords internal
 .SpecifyLinearTimeEffect <- function(time_intercept_param,
                                      time_trend_param){
   checkmate::expect_number(time_intercept_param, null.ok = FALSE)
@@ -73,6 +110,17 @@ SpecifyDataGeneratingModel <- function (n_study_periods = NULL,
   return(time_effect_param_vec)
 }
 
+#' @title Specify Categorical Time Effect Parameters
+#'
+#' @description
+#' Internal function to specify the parameters for a categorical time effect in the data generating model.
+#'
+#' @param n_study_periods Numeric. The number of study periods, must be at least 2.
+#' @param period_effect_param_vec Numeric vector. The period effect parameters for the time model.
+#'
+#' @return A named numeric vector with the time effect parameters.
+#'
+#' @keywords internal
 .SpecifyCategoricalTimeEffect <- function(n_study_periods, period_effect_param_vec){
   checkmate::expect_number(n_study_periods, lower = 2, null.ok = FALSE)
   checkmate::expect_numeric(period_effect_param_vec, any.missing = FALSE, null.ok = FALSE)
@@ -81,7 +129,19 @@ SpecifyDataGeneratingModel <- function (n_study_periods = NULL,
   return(period_effect_param_vec)
 }
 
-
+#' @title Specify Average or Linear Treatment Effect Parameters
+#'
+#' @description
+#' Internal function to specify the parameters for average or linear treatment effects in the data generating model.
+#'
+#' @param mli_study_flag Logical. Flag to indicate if the study is investigating a multi-level intervention.
+#' @param clust_trt_param Numeric. The treatment effect parameter at the cluster level.
+#' @param ind_trt_param Numeric. The treatment effect parameter at the individual level. Optional if `mli_study_flag` is FALSE.
+#' @param interaction_param Numeric. The interaction parameter for the treatment effect. Optional if `mli_study_flag` is FALSE.
+#'
+#' @return A named numeric vector with the treatment effect parameters.
+#'
+#' @keywords internal
 .SpecifyAvgOrLinearTrtEffect <- function(mli_study_flag,
                                    clust_trt_param,
                                    ind_trt_param,
@@ -104,7 +164,16 @@ SpecifyDataGeneratingModel <- function (n_study_periods = NULL,
 ## Input Checks
 ##
 ##############################################################################################
-
+  #' @title Check Inputs for Data Generating Model Specification
+  #'
+  #' @description
+  #' Internal function to validate the inputs for the `SpecifyDataGeneratingModel` function.
+  #' For details on the parameters, see the documentation for `SpecifyDataGeneratingModel`.
+  #'
+  #' @return NULL. Function returns NULL if all checks pass.
+  #'
+  #' @seealso \code{\link{SpecifyDataGeneratingModel}}
+  #' @keywords internal
 .CheckDGMInputs <- function(n_study_periods,
                             mli_study_flag,
                             time_model_type,
@@ -146,8 +215,16 @@ SpecifyDataGeneratingModel <- function (n_study_periods = NULL,
 
   return(NULL)
 }
-
-#' Check output of SpecifyDataGeneratingModel
+#' @title Check Inputs for Data Generating Model Specification
+#'
+#' @description
+#' Internal function to validate the output for the `SpecifyDataGeneratingModel` function.
+#' Checks that the output is a named numeric vector.
+#'
+#' @return NULL. Function returns NULL if all checks pass.
+#'
+#' @seealso \code{\link{SpecifyDataGeneratingModel}}
+#' @keywords internal
 .CheckDGMOutput <- function(dgm_param_vec){
   checkmate::expect_numeric(dgm_param_vec, any.missing = FALSE)
   checkmate::expect_names(names(dgm_param_vec))
