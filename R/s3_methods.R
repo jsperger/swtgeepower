7317bfe2cb502ccc4e767b480cc923e7fcb488e1
jsperger <j.sperger@gmail.com>
2024-05-28 04:34:56 -0400

    Delete S3 object stubs

--- R/s3_methods.R
+++
@@ -1,7 +0,0 @@
-print.working_cor_mat <- function(x) {
-  cat("Structure Type:", x$cor_structure_type, "\n")
-  cat("Dimensions:", dim(x$correlation_matrix), "\n")
-  cat("Sample Type:", x$sample_type, "\n")
-  cat("Working Correlation Matrix\n")
-  invisible(x)
-}
